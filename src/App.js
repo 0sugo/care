5b948c005a614ffcd23022cce1166f1979cf3485
Joseck Osugo <josugo26@gmail.com>
2024-07-26 23:59:30 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Contact from './pages/Contact'
 import Footer from './pages/Footer'
 import Header from './pages/Header'
 import Nav from './pages/Nav/Nav'
-qe
+q
 function App() {
   return (
     <div>
